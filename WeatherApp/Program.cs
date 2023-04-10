4436e230761dd12d7c773961464b020cb927baed
Bebetta Motondo <claramotondo2@yahoo.fr>
2023-04-10 13:46:13 -0500

    Weather App

---
+++ WeatherApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace WeatherApp;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string Key = File.ReadAllText("appsettings.json");
+
+        string APIKey = JObject.Parse(Key).GetValue("DefaultKey").ToString();
+
+        Console.WriteLine("Please enter your zipcode");
+
+        var zipCode = Console.ReadLine();
+
+        var apiCall = $"https://api.openweathermap.org/data/2.5/weather?zip={zipCode}&units=imperial&appid={APIKey}";
+
+        Console.WriteLine();
+
+        Console.WriteLine($"It is currently {WeatherMap.GetTemp(apiCall)} degrees F in your location.");
+
+    }
+}
+
